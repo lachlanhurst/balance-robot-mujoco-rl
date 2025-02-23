a6dc6355a38f8600f29f051e72cef39edb0eded3
Lachlan Hurst <lachlan.hurst@gmail.com>
2025-02-23 21:06:58 +1100

    narrow walled area to force more turning, tweak reward function
    it does kind of train ok, still needs improvement

--- src/balance_robot/envs/envMove05_v1.py
+++ src/balance_robot/envs/envMove05_v1.py
@@ -4,7 +4,12 @@ import numpy as np
 
 from scipy.spatial.transform import Rotation
 
-from balance_robot.envs.RobotMoveBaseEnv import RobotMoveBaseEnv, WHEEL_SPEED_DELTA_MAX
+from balance_robot.envs.RobotMoveBaseEnv import (
+    RobotMoveBaseEnv,
+    WHEEL_SPEED_DELTA_MAX,
+    YAW_MAX,
+    WHEEL_SPEED_MAX
+)
 
 
 class EnvMove05(RobotMoveBaseEnv):
@@ -12,11 +17,14 @@ class EnvMove05(RobotMoveBaseEnv):
     def __init__(self, **kwargs):
         RobotMoveBaseEnv.__init__(self, 'envMove05_v1.xml', **kwargs)
 
+        self.target_vel = 20.0
+
 
     def step(self, a):
         reward = self._get_reward()
 
-        self._step_wheel_speeds(15, 0)
+        self._step_wheel_speeds(a[0] * 20.0, a[1] * YAW_MAX)
+        # self._step_wheel_speeds(15, 0)
 
 
         mujoco.mj_step(self.model, self.data, nstep=self.frame_skip)
@@ -25,7 +33,12 @@ class EnvMove05(RobotMoveBaseEnv):
 
         self._update_camera_follow()
 
-        self.get_ray_hit_and_dist()
+        # dists, hits = self.get_ray_hit_and_dist()
+        # print("get_ray_hit_and_dist")
+        # print(dists)
+        # print(hits)
+
+
 
         # lidar_pos = self.data.body("front_indicator").xpos
         # lidar_pos = np.array(lidar_pos, dtype=np.float64)
@@ -81,49 +94,25 @@ class EnvMove05(RobotMoveBaseEnv):
             self.render()
 
         ob = self._get_obs()
+        # print(ob)
+        # print(reward)
         # truncation=False as the time limit is handled by the `TimeLimit` wrapper added during `make`
         return ob, reward, terminated, False, {}
 
 
     def _get_reward(self):
-        reward = 0.6
+        reward = 0.5
 
         pitch = self.get_pitch()
         wheel_speed = self.get_wheel_speed()
         dv = self.target_wheel_speed - wheel_speed
 
-        # penalty for not being vertical 
-        reward -= (abs(pitch) * 0.05)
+        reward += 0.03 * (wheel_speed - self.target_wheel_speed) / self.target_wheel_speed
 
-        MAX_DV = 40.0
-        max_dv = np.clip(dv, -MAX_DV, MAX_DV)
-        # will be -1 to 1
-        dv_n = max_dv / MAX_DV
-        dv_s = abs(dv_n)
-
-        reward -= (0.15 * dv_s)
-
-        if self.target_wheel_speed > 0 and self.target_wheel_speed > wheel_speed:
-            # then reward for leaning forward, needs to speed up forward
-            reward += (-1.0*pitch) * 10.0 * dv_s
-        elif self.target_wheel_speed < 0 and self.target_wheel_speed < wheel_speed:
-            # then reward for leaning backwards, needs to speed up backwards
-            reward += (1.0*pitch) * 10.0 * dv_s
-        elif self.target_wheel_speed > 0 and self.target_wheel_speed < wheel_speed:
-            # then reward for leaning backward, needs to slow down going forward
-            reward += (1.0*pitch) * 10.0 * dv_s
-        elif self.target_wheel_speed < 0 and self.target_wheel_speed > wheel_speed:
-            # then reward for leaning backwards, needs to speed up backwards
-            reward += (-1.0*pitch) * 10.0 * dv_s
-
-        # if self.loop_count % 10 == 0:
-        #     print(f"{dv_s}   {dv}  {reward}")
-
-        # small penalty for uneven wheel speeds (turning)
-        dyd = self.target_yaw - self.get_wheel_yaw()
-        # print(0.04 * abs(dyd))
-        reward -= (0.007 * abs(dyd))
-
+        dists, _ = self.get_ray_hit_and_dist()
+        for d_index in range(2,6):
+            d = dists[d_index]
+            reward += 0.15 * (d - 0.15) / 0.3
         return reward
 
 
